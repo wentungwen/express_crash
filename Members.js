f5bb23ce01ff321c3151fc47e6193dc0a36cd83e
wentungwen <wentungwen@gmail.com>
2021-03-11 23:26:48 +0800

    half done

---
+++ Members.js
@@ -0,0 +1,8 @@
+const members = [
+  { id: "1111", name: "personA", email: "1111@gmail.com", status: "active" },
+  { id: "2222", name: "personB", email: "2222@gmail.com", status: "inactive" },
+  { id: "3333", name: "personC", email: "3333@gmail.com", status: "active" },
+  { id: "4444", name: "personD", email: "4444@gmail.com", status: "inactive" },
+];
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const path = require("path");
+
+const members = require("./Members.js");
+const logger = require("./middleware/logger");
+
 const app = express();
 
-const members = [
-  { id: "1111", name: "personA", email: "1111@gmail.com", status: "active" },
-  { id: "2222", name: "personB", email: "2222@gmail.com", status: "inactive" },
-  { id: "3333", name: "personC", email: "3333@gmail.com", status: "active" },
-  { id: "4444", name: "personD", email: "4444@gmail.com", status: "inactive" },
-];
+// init logger
+app.use(logger);
+
+//get all members
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
 
 // set static folder path
 app.use(express.static(path.join(__dirname, "public")));
